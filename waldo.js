3ed1c7cb75ebb5a2d33d257b7a9883d3258fd8ac
abeprincec <abeprincec@gmail.com>
2018-05-09 22:50:40 +0000

    used forEach loop

--- waldo.js
+++ waldo.js
@@ -1,13 +1,21 @@
+// function findWaldo(arr, found) {
+//     for (var i = 0; i < arr.length; i++) {
+//       if (arr[i] === "Waldo") {
+//         found([i]);   // execute callback
+//       }
+//     }
+//   }
+
 function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
-        found([i]);   // execute callback
-      }
+  arr.forEach((element, index) => {
+    if (element === 'Waldo') {
+      found(index);
     }
-  }
-  
-  function actionWhenFound(index) {
-    console.log(`Found him at  ${index} index!`);
-  }
-  
-  findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+  });
+}
+
+function actionWhenFound(index) {
+  console.log(`Found him at  ${index} index!`);
+}
+
+findWaldo(['Alice', 'Bob', 'Waldo', 'Winston'], actionWhenFound);
